2646da5b244c235dbae2ff3fff206917585de135
unknown <izazahmed0177@gmail.com>
2022-09-28 22:52:51 +0600

    some component add

---
+++ src/components/MainActivity/MainActivity.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MainActivity = () => {
+    return (
+        <div className='main-activity-container'>
+            <div className='activity-list-container'>
+                <h1>jjjjj</h1>
+            </div>
+            <div className='activity-details-container'>
+                <h1>hhhhh</h1>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default MainActivity;
